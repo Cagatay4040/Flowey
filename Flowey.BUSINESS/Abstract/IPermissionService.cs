ac4fcab85811e3456386eb0e9ccac3eaa13618eb
Cagatay4040 <cagdulger@gmail.com>
2025-12-22 14:44:42 +0300

    Permission service added

---
+++ Flowey.BUSINESS/Abstract/IPermissionService.cs
@@ -0,0 +1,14 @@
+﻿using Flowey.CORE.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowey.BUSINESS.Abstract
+{
+    public interface IPermissionService
+    {
+        Task<bool> HasPermissionAsync(Guid userId, Guid projectId, params RoleType[] allowedRoles);
+    }
+}
---
+++ Flowey.BUSINESS/Concrete/PermissionManager.cs
@@ -0,0 +1,34 @@
+﻿using Flowey.BUSINESS.Abstract;
+using Flowey.CORE.Enums;
+using Flowey.DATACCESS.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flowey.BUSINESS.Concrete
+{
+    public class PermissionManager : IPermissionService
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public PermissionManager(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<bool> HasPermissionAsync(Guid userId, Guid projectId, params RoleType[] allowedRoles)
+        {
+            var projectUser = await _projectRepository.GetProjectUserAsync(projectId, userId);
+
+            if (projectUser == null)
+                return false;
+
+            if (allowedRoles == null || !allowedRoles.Any())
+                return true;
+
+            return allowedRoles.Contains((RoleType)projectUser.RoleId);
+        }
+    }
+}
