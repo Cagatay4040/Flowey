170cbad63253005afded806429cc15844eb66bc9
Cagatay4040 <cagdulger@gmail.com>
2025-12-22 19:04:58 +0300

    Interceptor user insert bug fixed

--- Flowey.DATACCESS/Services/AuditInterceptor.cs
+++ Flowey.DATACCESS/Services/AuditInterceptor.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Flowey.CORE.DataAccess.Abstract;
+using Flowey.DOMAIN.Model.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore.Diagnostics;
-using Microsoft.EntityFrameworkCore;
-using Flowey.CORE.DataAccess.Abstract;
 
 namespace Flowey.DATACCESS.Services
 {
@@ -26,23 +27,40 @@ namespace Flowey.DATACCESS.Services
             var context = eventData.Context;
             if (context == null) return base.SavingChangesAsync(eventData, result, cancellationToken);
 
+            var currentUserId = _currentUserService.GetUserId();
+
             foreach (var entry in context.ChangeTracker.Entries<IEntity>())
             {
                 if (entry.State == EntityState.Added)
                 {
                     entry.Entity.IsActive = true;
-                    entry.Entity.CreatedBy = _currentUserService.GetUserId().Value;
                     entry.Entity.CreatedDate = DateTime.Now;
+
+                    if (currentUserId.HasValue)
+                    {
+                        entry.Entity.CreatedBy = currentUserId.Value;
+                    }
+                    else
+                    {
+                        if (entry.Entity is User userEntity)
+                        {
+                            entry.Entity.CreatedBy = userEntity.Id;
+                        }
+                        else
+                        {
+                            entry.Entity.CreatedBy = Guid.Empty;
+                        }
+                    }
                 }
                 else if (entry.State == EntityState.Modified)
                 {
-                    entry.Entity.ModifiedBy = _currentUserService.GetUserId().Value;
+                    entry.Entity.ModifiedBy = currentUserId;
                     entry.Entity.ModifiedDate = DateTime.Now;
                 }
                 else if (entry.State == EntityState.Deleted)
                 {
                     entry.Entity.IsActive = false;
-                    entry.Entity.ModifiedBy = _currentUserService.GetUserId().Value;
+                    entry.Entity.ModifiedBy = currentUserId;
                     entry.Entity.ModifiedDate = DateTime.Now;
                 }
             }
