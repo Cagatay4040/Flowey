6a9c37c003eb4c89aa63309288a116ca8cf600d7
Cagatay4040 <cagdulger@gmail.com>
2026-01-10 23:30:11 +0300

    Html sanitizer added

--- Flowey.BUSINESS/Concrete/CommentManager.cs
+++ Flowey.BUSINESS/Concrete/CommentManager.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using AutoMapper;
+﻿using AutoMapper;
 using Flowey.BUSINESS.Abstract;
+using Flowey.BUSINESS.Constants;
 using Flowey.BUSINESS.DTO.Comment;
 using Flowey.CORE.Result.Abstract;
 using Flowey.CORE.Result.Concrete;
-using Flowey.BUSINESS.Constants;
 using Flowey.DATACCESS.Abstract;
 using Flowey.DOMAIN.Model.Concrete;
+using Ganss.Xss;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Flowey.BUSINESS.Concrete
 {
@@ -46,11 +47,14 @@ namespace Flowey.BUSINESS.Concrete
             if (!existingTask)
                 return new Result(ResultStatus.Error, Messages.TaskNotFound);
 
+            var cleanContent = SanitizeContent(dto.Content);
+
             var comment = _mapper.Map<Comment>(dto);
+            comment.Content = cleanContent;
 
             int effectedRow = await _commentRepository.AddAsync(comment);
 
-            if (effectedRow > 0) 
+            if (effectedRow > 0)
                 return new Result(ResultStatus.Success, Messages.CommentAdded);
 
             return new Result(ResultStatus.Error, Messages.CommentCreateError);
@@ -64,17 +68,21 @@ namespace Flowey.BUSINESS.Concrete
         {
             var existingComment = await _commentRepository.GetByIdAsync(dto.Id);
 
-            if (existingComment == null) 
+            if (existingComment == null)
                 return new Result(ResultStatus.Error, Messages.CommentNotFound);
+
+            var cleanContent = SanitizeContent(dto.Content);
 
             _mapper.Map(dto, existingComment);
 
-            int effectedRow =  await _commentRepository.UpdateAsync(existingComment);
+            existingComment.Content = cleanContent;
+
+            int effectedRow = await _commentRepository.UpdateAsync(existingComment);
 
             if (effectedRow > 0)
                 return new Result(ResultStatus.Success, Messages.CommentUpdated);
 
-            return new Result(ResultStatus.Success, Messages.CommentUpdateError);
+            return new Result(ResultStatus.Error, Messages.CommentUpdateError);
         }
 
         #endregion
@@ -94,6 +102,24 @@ namespace Flowey.BUSINESS.Concrete
                 return new Result(ResultStatus.Success, Messages.CommentDeleted);
 
             return new Result(ResultStatus.Error, Messages.CommentDeleteError);
+        }
+
+        #endregion
+
+        #region Helper Methods
+
+        private string SanitizeContent(string rawContent)
+        {
+            if (string.IsNullOrEmpty(rawContent)) return rawContent;
+
+            var sanitizer = new HtmlSanitizer();
+
+            sanitizer.AllowedTags.Add("table");
+            sanitizer.AllowedTags.Add("tr");
+            sanitizer.AllowedTags.Add("td");
+            sanitizer.AllowedTags.Add("img");
+
+            return sanitizer.Sanitize(rawContent);
         }
 
         #endregion
